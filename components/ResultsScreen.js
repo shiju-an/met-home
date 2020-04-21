b4d13c44fda78c488d533ae956821eac02037284
Joanna Wang <shiju.an362@gmail.com>
2020-04-20 22:28:18 -0700

    try to optimize flatlits results

--- components/ResultsScreen.js
+++ components/ResultsScreen.js
@@ -13,16 +13,17 @@ export default function ResultsScreen(props) {
       renderItem={({ item }) => (
         <View>
           <ResultEntry
+            keyExtractor={(item, index) => item.key}
             image={item.primaryImage}
             title={item.title}
           />
         </View>
       )}
-      removeClippedSubviews={true} // Unmount components when outside of window 
-      initialNumToRender={2} // Reduce initial render amount
+      removeClippedSubviews={true} // Unmount components when outside of window
+      initialNumToRender={3} // Reduce initial render amount
       maxToRenderPerBatch={1} // Reduce number in each render batch
       updateCellsBatchingPeriod={100} // Increase time between renders
-      windowSize={7} // Reduce the window size    
+      windowSize={7} // Reduce the window size
     />
   );
 }
